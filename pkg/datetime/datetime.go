b3a37223826e61137922a9617646870c58f9f9ca
lfernf <fernando.fferreira@mercadolivre.com>
2022-07-22 17:40:17 -0300

    WIP Datetime

--- pkg/datetime/datetime.go
+++ pkg/datetime/datetime.go
@@ -1,6 +1,30 @@
 package datetime
 
+import (
+	"joda-go/pkg/date"
+	"joda-go/pkg/localtime"
+)
+
 type Datetime interface {
 	Crop() interface{}
 	ToString() string
+}
+
+type dateTime struct {
+	date date.Date
+	time localtime.Time
+}
+
+func (d dateTime) ToString() string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d dateTime) Crop() interface{} {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewDateTime(day, month, year, hour, minute, second int) Datetime {
+	return &dateTime{}
 }
---
+++ pkg/datetime/datetime_test.go
@@ -0,0 +1,9 @@
+package datetime
+
+import (
+	"testing"
+)
+
+func Test_Time(t *testing.T) {
+	_ = NewDateTime(20, 1, 2022, 1, 2, 50)
+}
